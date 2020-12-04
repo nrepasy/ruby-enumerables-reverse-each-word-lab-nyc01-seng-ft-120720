821e1634d340212a54f8cb06a90912fb40beac7b
nick repasy <nrepasy@gmail.com>
2020-12-04 01:34:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,5 @@
 
 def reverse_each_word(sentence)
-  sentence.split.map {|word| word.reverse}.join(" ")
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
 
-def uses_collect(sentence)
-  sentence.collect
-end
